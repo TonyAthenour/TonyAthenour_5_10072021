4d56aa57da18667b5a399c445e35aafae360d776
TonyAthenour <tony.athenour@gmail.com>
2021-07-15 13:37:40 +0200

    v0_02 webpack loader: babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+/* webpack */
+
 module.exports = {
   mode: "production",
   entry: {
@@ -8,5 +10,33 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  }
+};
+
+/* webpack loader babel */
+
+module.exports = {
+  mode: "production",
+  entry: {
+    polyfill: "babel-polyfill",
+    app: "./src/index.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
